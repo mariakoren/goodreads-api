4087f78135cba77ecb0f05e9f86aef0d85bdd9b0
Maria Koren <maria.korenn@gmail.com>
2025-02-02 17:59:51 +0100

    firs secure endpoint

---
+++ src/main/java/com/example/goodreads/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.goodreads.controller;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping()
+    @PreAuthorize("hasRole('client_user')")
+    public String hello(){
+        return "hello user";
+    }
+
+    @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('client_admin')")
+    public String hello2(){
+        return "hello admin";
+    }
+}
