d6cf40fe7fe042f06a2c42b616ffeb275b5f169a
Maria Koren <maria.korenn@gmail.com>
2025-02-03 13:52:55 +0100

    username in demo

--- src/main/java/com/example/goodreads/controller/DemoController.java
+++ src/main/java/com/example/goodreads/controller/DemoController.java
@@ -1,7 +1,8 @@
 package com.example.goodreads.controller;
 
-
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,15 @@ public class DemoController {
 
     @GetMapping()
     @PreAuthorize("hasRole('client_user')")
-    public String hello(){
-        return "hello user";
+    public String hello(@AuthenticationPrincipal Jwt jwt) {
+        String username = jwt.getClaimAsString("preferred_username");
+        return "hello, " + username;
     }
 
     @GetMapping("/hello-2")
     @PreAuthorize("hasRole('client_admin')")
-    public String hello2(){
-        return "hello admin";
+    public String hello2(@AuthenticationPrincipal Jwt jwt) {
+        String username = jwt.getClaimAsString("preferred_username");
+        return "hello admin, "+ username;
     }
 }
--- src/main/java/com/example/goodreads/security/SecurityConfig.java
+++ src/main/java/com/example/goodreads/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.goodreads.security;
 
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,28 +14,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthConverter jwtAuthConverter;
 
-    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
-        this.jwtAuthConverter = jwtAuthConverter;
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests
+                                .anyRequest()
+                                .authenticated()
+                );
 
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
-                )
-
+        http
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
-                )
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthConverter)
+                        )
+                );
 
+        http
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
